1b1baf48c04ddf08577004e143eb26537be75861
sinilco <33919050+sinilco@users.noreply.github.com>
2022-12-30 13:06:16 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hpmetals</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero, itaque vero vel exercitationem unde, modi temporibus aliquid odio ut quasi quam? Maiores fugiat, laudantium pariatur ad necessitatibus eos. Omnis! <code></code> 
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
